a76ba7cd736ad54bec32506648ed3c587c9ae6c6
Faiz Abbas <faiz_abbas@hotmail.co.uk>
2019-02-13 11:45:30 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,10 +1,14 @@
 # Code your solution here!
 run_guessing_game
   user_input =""
+     random_number = rand(10)
   while user_input
    user_input = gets.chomp
-   random_number = rand(10)
+
    if user_input == random_number
       puts "You guessed the correct number!"
+      break
+    else
+      
     
   
